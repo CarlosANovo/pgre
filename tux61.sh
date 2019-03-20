81f4a5b903e0a3b1cb81e8832a11b4bd9843c26e
CarlosANovo <CarlosANovo@users.noreply.github.com>
2019-03-20 15:00:02 +0000

    DNS server config

--- tux61.sh
+++ tux61.sh
@@ -27,7 +27,7 @@ cp pgre-master/tux61/tux61.pgre.fe.up.pt.conf /etc/apache2/sites-available/tux61
 echo 'Enabling the Vitual Host...'
 a2ensite tux61.pgre.fe.up.pt
 systemctl restart apache2
-
+systemctl enable apache2
 
 echo ' === Installing the NTP server === '
 apt install -y ntp
@@ -43,9 +43,35 @@ cp pgre-master/tux61/vsftpd.conf /etc/vsftpd.conf
 systemctl restart vsftpd
 systemctl enable vsftpd
 
+# ADD USERS AND MAKE THEIR FOLDERS
+# useradd -m -c "Name" -s /bin/bash username
+# passwd username
+# echo "username" | tee -a /etc/vsftpd.userlist
+# mkdir /home/username/ftp
+# chown nobody:nogroup /home/username/ftp
+# chmod a-w /home/username/ftp
+# mkdir /home/username/ftp/files
+# chown -R username:username /home/username/ftp/files
+# chmod -R 0770 /home/username/ftp/files/
+
 echo ' === Installing DNS server === '
 apt install -y bind9 bind9utils
-# INSERT APROPRIATE FILES
+cp pgre-master/tux61/bind9 /etc/default/bind9
+cp /etc/bind/named.conf.options{,.backup}
+cp pgre-master/tux61/named.conf.options /etc/bind/named.conf.options
+cp /etc/bind/named.conf.local{,.backup}
+cp pgre-master/tux61/named.conf.local /etc/bind/named.conf.local
+echo 'Making the databases...'
+mkdir /etc/bind/zones
+cp pgre-master/tux61/db.1.16.172 /etc/bind/zones/db.1.16.172
+cp pgre-master/tux61/db.pgre.fe.up.pt /etc/bind/zones/db.pgre.fe.up.pt
+echo 'Checking correct configuration...'
+named-checkconf
+named-checkzone pgre.fe.up.pt /etc/bind/zones/db.pgre.fe.up.pt
+named-checkzone 1.16.172.in-addr.arpa /etc/bind/zones/db.1.16.172
+echo 'Restarting and enabling DNS server...'
+systemctl restart bind
+systemctl enable bind
 
 echo ' === Installing e-mail server === '
 
